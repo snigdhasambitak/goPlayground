f967c1af2ec02778642faa5fa58bc097d783eeb3
Snigdha Aryakumar <saryakumar@travix.com>
2022-01-29 23:13:02 +0100

    defer example

--- defer.go
+++ defer.go
@@ -7,11 +7,19 @@ import (
 
 func main(){
 
+	// defer count example
+	fmt.Println("count start")
+	for i:=0; i<10; i++{
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+
+	// defer file example
 	f, err := os.Create("file")
 	if err != nil{
 		panic("cannot create file")
 	}
 	defer f.Close()
 	fmt.Println("file closed")
-	
 }
