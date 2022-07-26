bbf6a4f2b15533ae4e0e1ac73c1b53201905c7c7
Snigdha Aryakumar <saryakumar@travix.com>
2022-07-26 19:48:22 +0200

    refactor defer example

--- defer.go
+++ defer.go
@@ -5,21 +5,31 @@ import (
 	"os"
 )
 
-func main(){
+func main() {
 
-	// defer count example
-	fmt.Println("count start")
-	for i:=0; i<10; i++{
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("done")
+	// use defer with count
+	defercount()
 
 	// defer file example
+	deferfile()
+
+}
+
+func defercount() {
+	fmt.Println("count start")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
+
+func deferfile() {
 	f, err := os.Create("file")
-	if err != nil{
+	if err != nil {
 		panic("cannot create file")
 	}
 	defer f.Close()
 	fmt.Println("file closed")
+
 }
